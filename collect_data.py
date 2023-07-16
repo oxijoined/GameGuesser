d75b686684b0231ef7b823135bccd25f85fca1da
okcu <dev@okcuu.ru>
2023-07-16 16:36:07 +0300

    upd

--- collect_data.py
+++ collect_data.py
@@ -25,16 +25,19 @@ async def requests():  # Определение асинхронной функ�
         rawg.Configuration(api_key={"key": RAWG_KEY})
     ) as api_client:
         api = rawg.GamesApi(api_client)
-        total_pages = 100
+        total_pages = 15
         with tqdm(total=total_pages, desc="Обработка страниц") as pbar_pages:
             for page in range(1, total_pages + 1):
-                games = await api.games_list(
-                    page_size=1000, page=page, ordering="-metacritic"
-                )  # Получение списка игр с использованием API
-                for game in games.results:  # Перебор игр в полученном списке
-                    game_obj = Game(game.id, game.name, game.background_image)
-                    games_list.append(game_obj)  # Добавление объекта Game в список игр
-                pbar_pages.update(1)  # Увеличение значения прогресс-бара
+                try:
+                    games = await api.games_list(
+                        page_size=1000, page=page, ordering="-metacritic,released", platforms="4,187,1,18,186,7"
+                    )  # Получение списка игр с использованием API
+                    for game in games.results:  # Перебор игр в полученном списке
+                        game_obj = Game(game.id, game.name, game.background_image)
+                        games_list.append(game_obj)  # Добавление объекта Game в список игр
+                    pbar_pages.update(1)  # Увеличение значения прогресс-бара
+                except:
+                    break
         json_data = json.dumps(
             [game.__dict__ for game in games_list], indent=4
         )  # Преобразование списка игр в JSON-формат
